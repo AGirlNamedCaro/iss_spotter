aae51ec8a2ad13693a4a68cc3d975e5268d24afe
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-17 00:40:39 +0000

    ISS spotter that is made with promises rather than callbacks

--- index.js
+++ index.js
@@ -34,10 +34,11 @@
 
 const printPassTimes = function(passTimes) {
   for(const pass of passTimes) {
-    const datetime = new Date(0);
+    const dateTime = new Date(pass['risetime'])
     datetime.setUTCSeconds(pass.risetime);
     const duration = pass.duraton;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+    
+    console.log(`Next pass at ${dateTime.toString()} for ${duration} seconds!`);
   }
 }
 
